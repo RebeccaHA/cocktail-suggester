d80ec416f577b9c66398cddbcb4a3c308f015bf0
RebeccaHA <rebeccahunt034@gmail.com>
2020-03-31 22:31:44 +0100

    created tables and migrations

--- cocktail-suggester/db/migrate/20200331184829_create_cocktail.rb
+++ cocktail-suggester/db/migrate/20200331184829_create_cocktail.rb
@@ -1,4 +1,12 @@
 class CreateCocktail < ActiveRecord::Migration
   def change
+    create_table :cocktail do |t|
+      t.string :name
+      t.string :description
+      t.string :ingredients
+      t.string :garnish
+      t.string :glassware
+      t.integer :method_time
+    end
   end
 end
