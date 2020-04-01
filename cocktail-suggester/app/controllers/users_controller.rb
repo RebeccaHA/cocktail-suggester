1cb054758bc810c7fef8ab1aed68958683bfd6eb
RebeccaHA <rebeccahunt034@gmail.com>
2020-04-01 19:08:10 +0100

    made error page and links back to login/signup

--- cocktail-suggester/app/controllers/users_controller.rb
+++ cocktail-suggester/app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UserController < ApplicationController
     end
 
     post'/login' do
-        @user = User.find_by(username: params[:username])
-        if @user !=nil && @user.password == params[:password]
-            session[:user_id] = @user.:id 
+        user = User.find_by(username: params[:username])
+        if user !=nil && user.password == params[:password]
+            session[:user_id] = user.id 
             redirect to '/users/show'
         end
         erb :'/users/error'
