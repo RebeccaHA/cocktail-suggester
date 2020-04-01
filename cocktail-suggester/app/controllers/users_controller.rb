f9e8a0e0e0c193bd8a43961d50357eab8c81e26b
RebeccaHA <rebeccahunt034@gmail.com>
2020-04-01 18:56:04 +0100

    added User form and get/post routes

--- cocktail-suggester/app/controllers/users_controller.rb
+++ cocktail-suggester/app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UserController < ApplicationController
+    get '/login' do
+    
+    erb :'/users/login'
+    end
 
+    post'/login' do
+        @user = User.find_by(username: params[:username])
+        if @user !=nil && @user.password == params[:password]
+            session[:user_id] = @user.:id 
+            redirect to '/users/show'
+        end
+        erb :'/users/error'
+    end
 end
