520fabcbb011da798832d3e6575b1168d6a458eb
RebeccaHA <rebeccahunt034@gmail.com>
2020-04-07 21:30:34 +0100

    fixed relationships - user has many cocktails

--- cocktail-suggester/app/models/cocktail.rb
+++ cocktail-suggester/app/models/cocktail.rb
@@ -1,6 +1,7 @@
 class Cocktail < ActiveRecord::Base
    
     has_and_belongs_to_many :stashes
+    belongs_to :user
   #  has_many :ingredients, through: :stash
     
 end
--- cocktail-suggester/app/models/user.rb
+++ cocktail-suggester/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_many :stashes
-    has_many :cocktails, through: :stashes
+    has_many :cocktails
     has_secure_password
     validates :username, presence: true, uniqueness: true, on: :create
     validates :password_digest, presence: true, uniqueness: true, on: :create
