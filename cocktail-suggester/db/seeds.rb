3812001799e1b3d1a43eedfaef0291b9d59c9806
RebeccaHA <rebeccahunt034@gmail.com>
2020-04-09 16:23:03 +0100

    added cocktail entries via csv

--- cocktail-suggester/db/seeds.rb
+++ cocktail-suggester/db/seeds.rb
@@ -3,14 +3,13 @@ require 'pry'
 require 'csv'
 
 csv_text = CSV.read("db/cocktails.csv")
-binding.pry
 csv = CSV.open('db/cocktails.csv', headers: :first_row).map(&:to_h)
 
     csv.each do |row|
         cocktail = Cocktail.new
         cocktail.name = row['name']
         cocktail.description = row['description']
-        cocktail.glasssare = row['glassware']
+        cocktail.glassware = row['glassware']
         cocktail.ingredients = row['ingredients']
         cocktail.method = row['method']
         cocktail.garnish = row['garnish']
